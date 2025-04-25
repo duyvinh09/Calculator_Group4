0ce574c327eb118712db7c3e62b7ec7b0dcafec9
nhatnguyen115 <nhatnguyen.110503@gmail.com>
2025-04-25 14:12:29 +0700

    copy

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,17 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package calculator;
 
-/**
- *
- * @author dinhduyvinh
- */
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Clipboard;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class Calculator {
-   
+
+    // Hàm tính toán biểu thức
+    public static double evaluate(String expr) {
+        expr = expr.replaceAll("√", "sqrt");
+        Expression expression = new ExpressionBuilder(expr)
+                .build();
+        return expression.evaluate();
+    }
+
+    // Hàm copy kết quả vào clipboard
+    public static void copyToClipboard(String text) {
+        StringSelection selection = new StringSelection(text);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, null);
+    }
+
+    // Hàm main test (tùy bạn giữ hay bỏ)
     public static void main(String[] args) {
         calculatorForm a = new calculatorForm();
         a.setVisible(true);
+
+        // Test biểu thức
+        String expr = "5 + (3 * 2) - √9";
+        double result = evaluate(expr);
+        System.out.println("Kết quả: " + result);
+
+        // Copy vào clipboard
+        copyToClipboard(String.valueOf(result));
     }
 }
